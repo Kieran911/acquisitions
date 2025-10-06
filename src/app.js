ab1f9d2ff764d04edaf535b8e63753e583a8a08a
Kieran911 <arinzekieran@gmail.com>
2025-10-06 14:43:06 +0100

    Implemented Lint and prettier

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from Maddox Acquisitions')
+  res.status(200).send('Hello from Maddox Acquisitions');
 });
 
-export default app
+export default app;
